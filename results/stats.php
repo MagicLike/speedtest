23b618ce4be2cd64969d7344a70663c70ec44fb5
MagicLike <contact@magiclike.net>
2024-07-12 12:43:21 +0200

    Moved and updated stats CSS
    - Moved stats CSS to it's own file
    - Darkened CSS
    - Made some font adjustments

--- results/stats.php
+++ results/stats.php
@@ -15,70 +15,7 @@ header('Pragma: no-cache');
 
 <head>
     <title>LibreSpeed - Stats</title>
-    <style type="text/css">
-        html,
-        body {
-            margin: 0;
-            padding: 0;
-            border: none;
-            width: 100%;
-            min-height: 100%;
-        }
-
-        html {
-            background-color: hsl(198, 72%, 35%);
-            font-family: "Segoe UI", "Roboto", sans-serif;
-        }
-
-        body {
-            background-color: #FFFFFF;
-            box-sizing: border-box;
-            width: 100%;
-            max-width: 70em;
-            margin: 4em auto;
-            box-shadow: 0 1em 6em #00000080;
-            padding: 1em 1em 4em 1em;
-            border-radius: 0.4em;
-        }
-
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-            font-weight: 300;
-            margin-bottom: 0.1em;
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        table {
-            margin: 2em 0;
-            width: 100%;
-        }
-
-        table,
-        tr,
-        th,
-        td {
-            border: 1px solid #AAAAAA;
-        }
-
-        th {
-            width: 6em;
-        }
-
-        td {
-            word-break: break-all;
-        }
-
-        div {
-            margin: 1em 0;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="/css/stats.css">
 </head>
 
 <body>
